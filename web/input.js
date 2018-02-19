cd4ddc157a946fdfbe3ed7a7d82655e5d4aa5677
James Rowen <jamesrowen@gmail.com>
2018-02-19 11:32:23 -0800

    add styling and animation of control panel, esc to toggle

---
+++ web/input.js
@@ -0,0 +1,28 @@
+function changeSetting(setting, value) {
+  const val = parseInt(value);
+  switch(setting) {
+    case 'tileSize':
+      tileSize = val;
+      break;
+    case 'tilePadding':
+      tilePadding = val;
+      break;
+    case 'gridX':
+      gridSize.x = val;
+      break;
+    case 'gridY':
+      gridSize.y = val;
+      break;
+    case 'curveIterations':
+      curveIterations = val;
+      break;
+  }
+}
+
+function keyPressed() {
+  switch(keyCode) {
+    case 27:
+      document.getElementById('controls').classList.toggle('closed');
+      break;
+  }
+}
