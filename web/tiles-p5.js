6297b931c268fd6848b1c954a98d80255b34ff94
James Rowen <jamesrowen@gmail.com>
2018-02-19 11:41:49 -0800

    getting tile padding working

--- web/tiles-p5.js
+++ web/tiles-p5.js
@@ -2,7 +2,7 @@
 var tileColors;
 var tileSize = 48;
 var tilePadding = 0;
-var curveIterations = 15;
+var curveIterations = 10;
 var gridSize = {x: 24, y: 15};
 
 function setup() {
@@ -21,23 +21,24 @@ function draw() {
   for (let y = 0; y < gridSize.y; y++) {
     for (let x = 0; x < gridSize.x; x++) {
       const gridfactor = (x + y) / 4;
-      translate(tileSize * x, tileSize * y);
+      translate(tileSize * x + tilePadding, tileSize * y + tilePadding);
       drawTile();
-      translate(-tileSize * x, -tileSize * y);
+      translate(-(tileSize * x + tilePadding), -(tileSize * y + tilePadding));
     }
   }
 }
 
 function drawTile(x, y) {
+  const shapeSize = tileSize - tilePadding * 2;
   noStroke();
   // draw two halves, first upper left then bottom right
   for (let i = 0; i < 2; i++) {
     fill(tileColors[i]);
     beginShape();
-    vertex(tileSize * i + tilePadding * Math.pow(-1, i), tileSize * i + tilePadding * Math.pow(-1, i));
+    vertex(shapeSize * i, shapeSize * i);
     for (let j = 0; j <= curveIterations; j++) {
-      vertex(j / curveIterations * (tileSize - tilePadding * 2),
-        (cos(j / curveIterations * PI) + 1) / 2 * (tileSize - tilePadding * 2));
+      vertex(j / curveIterations * shapeSize,
+        (cos(j / curveIterations * PI) + 1) / 2 * shapeSize);
     }
     endShape(CLOSE);
   }
