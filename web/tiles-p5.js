6e541c146e45d3d04fdc95f3abd06dcfcef9a2c1
James Rowen <jamesrowen@gmail.com>
2018-02-19 11:39:38 -0800

    add spinning, with orbit mode - add playback controls - add keyboard shortcuts

--- web/tiles-p5.js
+++ web/tiles-p5.js
@@ -1,13 +1,23 @@
-// global settings
+// settings
 var tileColors;
-var tileSize = 48;
+var tileSize = 10;
 var tilePadding = 0;
 var curveIterations = 10;
-var gridSize = {x: 24, y: 15};
+var gridSize = {x: 30, y: 30};
+var speed = 40;
+var rewind = 1;
+var pause = false;
+var camX = 0;
+var camY = 0;
+
+// internal globals
+var lastTime = 0;
+var orbitPos = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tileColors = [color(0, 0, 0), color(255, 255, 255)];
+  tileColors = [color(0, 0, 0, 160), color(255, 255, 255, 160)];
+  noStroke();
 }
 
 function windowResized() {
@@ -15,22 +25,31 @@ function windowResized() {
 }
 
 function draw() {
+  const time = millis() / 1000.0;
+  document.getElementById('message').textContent = "orbit: " + parseInt((orbitPos - 1) * 10);
+  const elapsed = pause ? 0 : time - lastTime;
+  lastTime = time;
+  orbitPos += elapsed / speed * rewind;
   resetMatrix();
-  background('#eee');
-  translate(30, 30);
+  background('#f0f0f0');
+
+  translate(camX, camY);
   for (let y = 0; y < gridSize.y; y++) {
     for (let x = 0; x < gridSize.x; x++) {
-      const gridfactor = (x + y) / 4;
+      const tilePct = (x + y * gridSize.x) / (gridSize.x - 1) / (gridSize.y - 1);
+      // tileColors[0] = color(255 - 105 * x / gridSize.x, 255, 255 - 105 * y / gridSize.y, 200);
+      // tileColors[1] = color(205 * x / gridSize.x + 50, 205 * y / gridSize.y + 50, 255, 200);
       translate(tileSize * x + tilePadding, tileSize * y + tilePadding);
+      rotate(orbitPos * tilePct);
       drawTile();
+      rotate(-tilePct);
       translate(-(tileSize * x + tilePadding), -(tileSize * y + tilePadding));
     }
   }
 }
 
-function drawTile(x, y) {
+function drawTile() {
   const shapeSize = tileSize - tilePadding * 2;
-  noStroke();
   // draw two halves, first upper left then bottom right
   for (let i = 0; i < 2; i++) {
     fill(tileColors[i]);
