42182f9a834935b6e2d4aa490ca8f2f1823d70af
James Rowen <jamesrowen@gmail.com>
2018-02-22 18:42:41 -0800

    custom slider styles and sliders now display value

--- web/settings.js
+++ web/settings.js
@@ -5,9 +5,9 @@ var settings = {
   // playback
   'rewind': {default: false, parse: parseBool, attr: 'value'},
   'playing': {default: true, parse: parseBool, attr: 'value'},
-  'spinSpeed': {default: 40, parse: parseInt, attr: 'value'},
-  'patternSpeed': {default: 40, parse: parseInt, attr: 'value'},
-  'orbitSpeed': {default: 40, parse: parseInt, attr: 'value'},
+  'spinSpeed': {default: 40, parse: parseInt, attr: 'value', type: 'slider'},
+  'patternSpeed': {default: 40, parse: parseInt, attr: 'value', type: 'slider'},
+  'orbitSpeed': {default: 40, parse: parseInt, attr: 'value', type: 'slider'},
   // colors
   'color1a': {default: '#000000', parse: x => x, attr: 'value'},
   'color2a': {default: '#ffffff', parse: x => x, attr: 'value'},
@@ -17,20 +17,20 @@ var settings = {
   'g1y': {default: false, parse: parseBool, attr: 'checked'},
   'g2x': {default: false, parse: parseBool, attr: 'checked'},
   'g2y': {default: true, parse: parseBool, attr: 'checked'},
-  'c1alpha': {default: 160, parse: parseInt, attr: 'value'},
-  'c2alpha': {default: 160, parse: parseInt, attr: 'value'},
+  'c1alpha': {default: 160, parse: parseInt, attr: 'value', type: 'slider'},
+  'c2alpha': {default: 160, parse: parseInt, attr: 'value', type: 'slider'},
   'bgColor': {default: '#f0f0f0', parse: x => x, attr: 'value'},
   // spin mode
   'spin': {default: 0, parse: parseFloat},
-  'tileSize' : {default: 30, parse: parseInt, attr: 'value'},
-  'tileScale': {default: 1, parse: parseFloat, attr: 'value'},
-  'margin': {default: 30, parse: parseInt, attr: 'value'},
-  'shapeIterations': {default: 7, parse: parseInt, attr: 'value'},
+  'tileSize' : {default: 30, parse: parseInt, attr: 'value', type: 'slider'},
+  'tileScale': {default: 1, parse: parseFloat, attr: 'value', type: 'slider'},
+  'margin': {default: 30, parse: parseInt, attr: 'value', type: 'slider'},
+  'shapeIterations': {default: 7, parse: parseInt, attr: 'value', type: 'slider'},
   // orbit mode
   'orbit': {default: 0, parse: parseFloat},
-  'radius': {default: 20, parse: parseInt, attr: 'value'},
-  'bodies': {default: 30, parse: parseInt, attr: 'value'},
-  'zoom': {default: 10, parse: parseInt, attr: 'value'},
+  'radius': {default: 20, parse: parseInt, attr: 'value', type: 'slider'},
+  'bodies': {default: 30, parse: parseInt, attr: 'value', type: 'slider'},
+  'zoom': {default: 10, parse: parseInt, attr: 'value', type: 'slider'},
   'camX': {default: 0, parse: parseInt},
   'camY': {default: 0, parse: parseInt}
 };
@@ -47,6 +47,10 @@ function updateSetting(name, value) {
     el(name).setAttribute(settings[name].attr, val);
     if (settings[name].attr == 'value')
       el(name).value = val;
+  }
+
+  if (settings[name].type == 'slider') {
+    el(name).parentNode.querySelector('span').textContent = val;
   }
 }
 
