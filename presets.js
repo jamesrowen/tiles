1ec72f5efecc9d8382c8a254ad7770c83672dfdf
James Rowen <jamesrowen@gmail.com>
2018-03-05 14:18:19 -0800

    - move input element styles to new file - better color preset layout

--- presets.js
+++ presets.js
@@ -9,7 +9,7 @@ var settingsByMode = {
 function exportPreset(type) {
   let settingsToExport = (type == 'color') ? [] : settingsByMode.all;
   settingsToExport = settingsToExport.concat(settingsByMode[type]);
-  let strings = settingsToExport.map(s => s + ': ' + JSON.stringify(this[s]));
+  let strings = settingsToExport.map(s => s + ':' + JSON.stringify(this[s]));
   el('exportText').innerHTML = '{' + strings.join(',') + '},';
 }
 
@@ -21,7 +21,7 @@ function loadPreset(i) {
 
 var presets = [
   {mode: "spin",playing: true,rewind: false,shapeIterations: 8,spinSpeed: 57,spin: 4.950363636429591,spinY: 0,spinYSpeed: 0,spinX: 0,spinXSpeed: 0,tileSize: 110,tileScale: 1.48,margin: 11},
-  {mode: "pattern",playing: true,rewind: false,shapeIterations: 8,patternSpeed: 37,pattern: 0,curPattern: "teeth",loopTrans: true,curTransitions: [["v-zags","center-circle",0.3891992000011241]],tileSize: 40,tileScale: 2,animationLength: 0.37,shortRotations: true,rotX: false,rotY: false},
+  {mode: "pattern",playing: true,rewind: false,shapeIterations: 8,patternSpeed: 37,pattern: 0,curPattern: "teeth",loopTrans: true,curTransitions: [["v-zags","center-circle",0.3891992000011241]],tileSize: 56,tileScale: 2,animationLength: 0.37,shortRotations: true,rotX: false,rotY: false},
   {mode: "orbit",playing: true,rewind: true,shapeIterations: 8,orbitSpeed: 24,orbit: 3.7467108700007334,curl: 2,bodies: 620,zoom: 43,growth: 49,camX: 1158.0643253063342,camY: 348.5731432706755},
   {mode: "orbit",playing: true,rewind: false,shapeIterations: 8,orbitSpeed: 14,orbit: 0.2139395200003869,curl: 6,bodies: 690,zoom: 70,growth: 100,camX: 911.1633102231315,camY: 1.3978655948883443},
   {color1a: "#000000",color2a: "#ffffff",color1b: "#324c75",color2b: "#fff9d9",c1mode: 0,c2mode: 1,c1alpha: 0.77,c2alpha: 0.4,bgColor: "#2e312e"},
